80ed52ea3dc607350ed68d084da86c163e24e6a9
Michael Tiago Barroso <michael.barroso@itaipuparquetec.org.br>
2025-11-10 22:08:30 -0300

    feat-clube-mais-backend

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 
     <div class="mt-2 mb-4 text-center">
         <a class="items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"  href="{{ route('login.sso') }}">
-            Login com SSO
+            Login com SSO Clube +
         </a>
     </div>
     <form method="POST" action="{{ route('login') }}">
